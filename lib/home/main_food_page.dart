5f56ccae2d21650b2ba051e6d5638aff03075841
Ilham Riana <ilhamriana3@gmail.com>
2022-05-06 14:36:56 +0700

    committed

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myfooddelivery/utils/colors.dart';
+import 'package:myfooddelivery/widgets/big_text.dart';
+import 'package:myfooddelivery/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            //first container // top widgets
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(children: [
+                    BigText(text: "Indonesia"),
+                    Row(
+                      children: [
+                        SmallText(text: "Jakarta"),
+                        Icon(Icons.arrow_drop_down_sharp)
+                      ],
+                    )
+                  ]),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = Colors.blue;
+  static final Color iconColor1 = const Color(0xFFffd28d);
+  static final Color iconColor2 = const Color(0xFFccc7c5);
+  static final Color paraColor = const Color(0xFFccc7c5);
+  static final Color buttonBackgroundColor = Colors.black;
+  static final Color signColor = const Color(0xFFccc7c5);
+  static final Color titleColor = const Color(0xFFccc7c5);
+  static final Color mainBlackColor = Colors.black;
+  static final Color yellowColor = Colors.yellow;
+}
